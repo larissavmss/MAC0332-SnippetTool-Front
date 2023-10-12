05e87b6a37dae962834a8cfcdd1c6298aaa3bba7
Kaiqoliver <kakadeoliveira@outlook.com>
2023-10-12 19:21:58 -0300

    Guardando o conteúdo da barra de pesquisas em uma variável e printando ela no console

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,41 +1,88 @@
 import User from "../User/User";
-import { FlexButtons, HeaderContainer, IconMenu, LeftContainer, Logo, MenuButton, SearchBar, SignIn, SignUp } from "./styles"
-import menu from "../../images/menu.png"
-import logo from "../../images/logobranca.png"
+import {
+  FlexButtons,
+  HeaderContainer,
+  IconMenu,
+  LeftContainer,
+  Logo,
+  MenuButton,
+  SearchBar,
+  SignIn,
+  SignUp,
+  LupinhaIcon, // Import LupinhaIcon
+} from "./styles";
+import menu from "../../images/menu.png";
+import logo from "../../images/logobranca.png";
+import lupinha from "../../images/lupinha_3.png"; // Import the lupinha image
 import { useState } from "react";
 import Menu from "../Menu/Menu";
 import { Link } from "react-router-dom";
 
 const Header = () => {
-    const [openMenu, setOpenMenu] = useState(false);
+  const [openMenu, setOpenMenu] = useState(false);
+  const [searchText, setSearchText] = useState("");
 
-    const userIsLogged = false; // TODO: pegar variavel global quando fizer autenticacao (Larissa)
+  const userIsLogged = false;
 
-    const toggleMenu = () => {
-        setOpenMenu(!openMenu);
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // Perform the search action with the searchText
+    console.log("Performing search with text: " + searchText);
+  };
+
+  const handleEnterKeyPress = (event) => {
+    if (event.key === "Enter") {
+      // Perform the search action with the searchText
+      console.log("Performing search with text: " + searchText);
     }
-    console.log(openMenu);
-    return (
-        <div>
-            <HeaderContainer>
-                <LeftContainer>
-                    <MenuButton onClick={toggleMenu}>
-                        <IconMenu src={menu} alt="Menu"/>
-                    </MenuButton>
-                    <Logo src={logo} alt="Ícone ST"/>
-                    <SearchBar type="text" placeholder="Pesquisar"/>
-                </LeftContainer>
-                {userIsLogged? <User/> : 
-                <FlexButtons>
-                    <Link to="/sign-in" style={{textDecoration: 'none'}}> <SignIn> Sign In </SignIn> </Link>
-                    <Link to="/sign-up" style={{textDecoration: 'none'}}> <SignUp> Sign Up </SignUp> </Link>
-                </FlexButtons>
-                }
-            </HeaderContainer>
-            {openMenu && (<Menu/>)}
-        </div>
-    )
-}
-// TODO: Colocar icone de pesquisa na barra (Cassio)
+  };
+
+  return (
+    <div>
+      <HeaderContainer>
+        <LeftContainer>
+          <MenuButton onClick={toggleMenu}>
+            <IconMenu src={menu} alt="Menu" />
+          </MenuButton>
+          <Logo src={logo} alt="Ícone ST" />
+          <SearchBar>
+            <LupinhaIcon
+              src={lupinha}
+              alt="Search"
+              onClick={handleSearch}
+            />
+            <input
+              type="text"
+              placeholder="Pesquisar"
+              value={searchText}
+              onChange={handleSearchChange}
+              onKeyPress={handleEnterKeyPress}
+            />
+          </SearchBar>
+        </LeftContainer>
+        {userIsLogged ? (
+          <User />
+        ) : (
+          <FlexButtons>
+            <Link to="/sign-in" style={{ textDecoration: "none" }}>
+              <SignIn> Sign In </SignIn>
+            </Link>
+            <Link to="/sign-up" style={{ textDecoration: "none" }}>
+              <SignUp> Sign Up </SignUp>
+            </Link>
+          </FlexButtons>
+        )}
+      </HeaderContainer>
+      {openMenu && <Menu />}
+    </div>
+  );
+};
 
 export default Header;
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,76 +2,82 @@ import styled from "styled-components";
 import { grey } from "../../utils/constants/constants";
 
 export const HeaderContainer = styled.div`
-background-color: ${grey};
-color: #fff;
-padding: 10px;
-display: flex;
-justify-content: space-between;
-align-items: center;
+  background-color: ${grey};
+  color: #fff;
+  padding: 10px;
+  display: flex;
+  justify-content: space between;
+  align-items: center;
 `;
 
 export const LeftContainer = styled.div`
-display:flex;
-justify-content: left;
-width: 70%;
-align-items:center;
+  display: flex;
+  justify-content: left;
+  width: 70%;
+  align-items: center;
 `;
 
 export const IconMenu = styled.img`
-cursor: pointer;
-margin-left:20px;
-height: 25px;
+  cursor: pointer;
+  margin-left: 20px;
+  height: 25px;
 `;
 
 export const Logo = styled.img`
-margin: 0 40px;
-width: 90px;
-height: auto;
+  margin: 0 40px;
+  width: 90px;
+  height: auto;
 `;
 
-export const SearchBar = styled.input`
-padding: 5px 5px 5px 15px;
-background-color: #666;
-border-radius: 5px;
-border: none;
-margin: 0 10px;
-width: 60%;
-height:24px;
-font-size: 1.0em;
-color:white;
+export const SearchBar = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 5px 10px;
+  background-color: #666;
+  border-radius: 5px;
+  border: none;
+  margin: 0 10px;
+  width: 60%;
+  height: 24px;
+  font-size: 1.0em;
+  color: white;
+`;
+
+export const LupinhaIcon = styled.img`
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+  margin-right: 10px; // Adjust margin as needed
 `;
 
 export const MenuButton = styled.button`
-background: none;
-border: none;
-cursor: pointer;
+  background: none;
+  border: none;
+  cursor: pointer;
 `;
 
 export const SignIn = styled.div`
-padding: 10px;
-color: #ddd;
-transition: 0.25s;
-&:hover {
-    transition: 0.25s;
+  padding: 10px;
+  color: #ddd;
+  transition: 0.25s;
+  &:hover {
     color: #999;
-}
-
+  }
 `;
 
 export const SignUp = styled.div`
-background-color: #ddd;
-padding: 10px;
-border-radius: 5px;
-color: ${grey};
-transition: 0.25s;
-&:hover {
-    transition: 0.25s;
+  background-color: #ddd;
+  padding: 10px;
+  border-radius: 5px;
+  color: ${grey};
+  transition: 0.25s;
+  &:hover {
     background-color: #999;
-}
+  }
 `;
 
 export const FlexButtons = styled.div`
-display: flex;
-justify-content: space-between;
-width: 160px;
-`
+  display: flex;
+  justify-content: space-between;
+  width: 160px;
+`;
