244fe85699792bb981cb9d2c8e0d8850400028ae
Kaique Nunes de Oliveira <koliver@go.linux.ime.usp.br>
2023-12-12 09:38:53 -0300

    Beginning the user.js service. Still need to understand how to fetch from the API

--- src/services/user.js
+++ src/services/user.js
@@ -1,7 +1,27 @@
-export const registerUser = (userInfo) => {
-
+export const registerUser = async (userInfo) => {
+    let userRegistered = false;
+    try {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'},
+            body: JSON.stringify(userInfo)
+        }
+        const response = await fetch("http://localhost:8080/user", requestOptions);
+        if(response.ok){
+            userRegistered = true;
+        } else {
+            throw new Error("Failed to create user");
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        return userRegistered;
+    }
 }
 
 export const login = (loginInfo) => {
-    
+    let user = null;
+    try {
+        const response = await fetch("http://localhost:8080/user")
+    }
 }
