31c7562a89fec77d65a57d62a4b4254d154bcf8f
larissavmss <larissavitoriamedeiros@usp.br>
2023-12-13 20:36:38 -0300

    fix empty snippet object

--- src/utils/constants/emptySnippet.js
+++ src/utils/constants/emptySnippet.js
@@ -1,9 +1,7 @@
 const emptySnippet = {
     name: "",
     content: "",
-    creation_date: new Date().toLocaleDateString(),
-    last_modification: new Date().toLocaleDateString(),
-    tags: []
+    tag: []
 }
 
 export default emptySnippet;
